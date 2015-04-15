e7f543137dae400f1d0854302886116123d9d42d
Tony Lim <atomictheorist@gmail.com>
2015-04-14 19:34:05 -0700

    Add word class

---
+++ word.cpp
@@ -0,0 +1,13 @@
+/**
+ * word.cpp
+ * Copyright (C) 2015 Tony Lim <atomictheorist@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include "word.h"
+namespace NLP
+{
+    
+} /* NLP */
+
---
+++ word.h
@@ -0,0 +1,29 @@
+/**
+ * word.h
+ * Copyright (C) 2015 Tony Lim <atomictheorist@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#ifndef WORD_H
+#define WORD_H
+
+#include "Tokenizer/stokenize.h"
+
+namespace NLP
+{
+
+class Word
+{
+    private:
+        /* data */
+
+    public:
+        Word();
+        virtual ~Word();
+}; /* -----  end of class Word  ----- */
+        
+} /* NLP */
+
+
+#endif /* !WORD_H */
